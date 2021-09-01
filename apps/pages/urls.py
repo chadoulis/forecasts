7cf6056796c455c1104493ce4f7405f0eff7557c
dhmm <dhmm@outlook.com>
2021-09-01 11:31:35 +0300

    login page added

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', TemplateView.as_view(template_name="page/Home.html")),
     path('about/', TemplateView.as_view(template_name="page/About.html")),
+    path('login/', TemplateView.as_view(template_name="page/Login.html")),
     path('register/', TemplateView.as_view(template_name="page/Register.html")),
     path('team/', TemplateView.as_view(template_name="page/Team.html")),
     path('terms-and-conditions/', TemplateView.as_view(template_name="page/TermsAndConditions.html")),
