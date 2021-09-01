85b14435b222a490cf8b29e62404b80ce97d27fb
dhmm <dhmm@outlook.com>
2021-09-01 12:03:32 +0300

    account related pages created ( account, account groups, account settings )

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -3,16 +3,26 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    #Gerenal Pages
     path('', TemplateView.as_view(template_name="page/Home.html")),
     path('about/', TemplateView.as_view(template_name="page/About.html")),
+    path('team/', TemplateView.as_view(template_name="page/Team.html")),
+    path('terms-and-conditions/', TemplateView.as_view(template_name="page/TermsAndConditions.html")),
+    path('privacy-policy/', TemplateView.as_view(template_name="page/PrivacyPolicy.html")),
+
+    #Registration and password pages
     path('login/', TemplateView.as_view(template_name="page/Login.html")),
     path('register/', TemplateView.as_view(template_name="page/Register.html")),
     path('forgot-password/', TemplateView.as_view(template_name="page/ForgotPassword.html")),    
     path('reset-password/<str:TempToken>', TemplateView.as_view(template_name="page/ResetPassword.html")),                        
     path('confirm-account/<str:TempToken>', TemplateView.as_view(template_name="page/ConfirmAccount.html")),                        
-    path('team/', TemplateView.as_view(template_name="page/Team.html")),
-    path('terms-and-conditions/', TemplateView.as_view(template_name="page/TermsAndConditions.html")),
-    path('privacy-policy/', TemplateView.as_view(template_name="page/PrivacyPolicy.html"))
+    
+    #Account pages    
+    path('account/', TemplateView.as_view(template_name="page/Account.html")),
+    path('account/groups/', TemplateView.as_view(template_name="page/AccountGroups.html")),
+    path('account/settings/', TemplateView.as_view(template_name="page/AccountSettings.html")),
 
+
+  
     
 ]
