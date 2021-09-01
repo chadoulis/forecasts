85243e5781ef62c3d97ab336d81a58d3534a7d67
dhmm <dhmm@outlook.com>
2021-09-01 11:20:39 +0300

    terms page added

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="page/Home.html")),
     path('about/', TemplateView.as_view(template_name="page/About.html")),
     path('register/', TemplateView.as_view(template_name="page/Register.html")),
-    path('team/', TemplateView.as_view(template_name="page/Team.html"))
+    path('team/', TemplateView.as_view(template_name="page/Team.html")),
+    path('terms/', TemplateView.as_view(template_name="page/Terms.html"))
 ]
