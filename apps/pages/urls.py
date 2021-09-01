17c38226e44a96d4cce3ecfc2f7cde0807644dca
dhmm <dhmm@outlook.com>
2021-09-01 11:28:23 +0300

    privacy policy page added

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="page/About.html")),
     path('register/', TemplateView.as_view(template_name="page/Register.html")),
     path('team/', TemplateView.as_view(template_name="page/Team.html")),
-    path('terms-and-conditions/', TemplateView.as_view(template_name="page/TermsAndConditions.html"))
+    path('terms-and-conditions/', TemplateView.as_view(template_name="page/TermsAndConditions.html")),
+    path('privacy-policy/', TemplateView.as_view(template_name="page/PrivacyPolicy.html"))
+
+    
 ]
