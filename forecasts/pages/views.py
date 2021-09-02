7fb3d38692d452ebb252185373ec74c3e704c29e
dhmm <dhmm@outlook.com>
2021-09-02 11:23:59 +0300

    general pages added : team, terms-and-conditions, privacy-policy

--- forecasts/pages/views.py
+++ forecasts/pages/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-class AboutView(TemplateView):
-    template_name = "main_templates/About.html"
-
 
 class HomeView(TemplateView):
     template_name = "main_templates/Home.html"
+
+class AboutView(TemplateView):
+    template_name = "main_templates/About.html"
+
+class TeamView(TemplateView):
+    template_name = "main_templates/Team.html"
+
+class TermsAndConditionsView(TemplateView):
+    template_name = "main_templates/TermsAndConditions.html"
+  
+class PrivacyPolicyView(TemplateView):
+    template_name = "main_templates/PrivacyPolicy.html"
