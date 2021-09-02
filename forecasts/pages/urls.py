2e71c28bd435527832628fea0d192ac842f6da52
dhmm <dhmm@outlook.com>
2021-09-02 14:34:23 +0300

    general pages added

--- forecasts/pages/urls.py
+++ forecasts/pages/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from forecasts.pages.views import AboutView, HomeView
+from forecasts.pages.views import HomeView, AboutView, TeamView , TermsAndConditionsView , PrivacyPolicyView
 
 urlpatterns = [
+    #General Pages
     path('', HomeView.as_view()),
     path('about/', AboutView.as_view()),
+    path('team/', TeamView.as_view()),
+    path('terms-and-conditions/', TermsAndConditionsView.as_view()),
+    path('privacy-policy/', PrivacyPolicyView.as_view()),
 ]
