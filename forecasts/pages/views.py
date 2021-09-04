1c60ec9208999935e5681cd3af31f433fdff5751
Alex Costantino <alex.costantino.app@gmail.com>
2021-09-04 12:42:28 +0300

    1) "main_templates" rename to "page"  Reason: 	a) The word "main" is not explicit. For everyone means something else. For exaple it could comfused with a root document 	b) The word "page" describes what actual is 	c) In the future we will have "composed" templates for other things like emails
    2) Renaming components for simplicity
    3) Renaming classes after the agreement with mutlu

--- forecasts/pages/views.py
+++ forecasts/pages/views.py
@@ -4,52 +4,52 @@ from django.views.generic import TemplateView
 
 #General views
 class HomeView(TemplateView):
-    template_name = "main_templates/Home.html"
+    template_name = "page/Home.html"
 class AboutView(TemplateView):
-    template_name = "main_templates/About.html"
+    template_name = "page/About.html"
 class TeamView(TemplateView):
-    template_name = "main_templates/Team.html"
+    template_name = "page/Team.html"
 class TermsAndConditionsView(TemplateView):
-    template_name = "main_templates/TermsAndConditions.html"  
+    template_name = "page/TermsAndConditions.html"  
 class PrivacyPolicyView(TemplateView):
-    template_name = "main_templates/PrivacyPolicy.html"
+    template_name = "page/PrivacyPolicy.html"
 
 #Registration and password views
 class LoginView(TemplateView):
-    template_name = "main_templates/Login.html"
+    template_name = "page/Login.html"
 class RegisterView(TemplateView):
-    template_name = "main_templates/Register.html"
+    template_name = "page/Register.html"
 class ForgotPasswordView(TemplateView):
-    template_name = "main_templates/ForgotPassword.html"
+    template_name = "page/ForgotPassword.html"
 class ResetPasswordView(TemplateView):
-    template_name = "main_templates/ResetPassword.html"
+    template_name = "page/ResetPassword.html"
 class ConfirmAccountView(TemplateView):
-    template_name = "main_templates/ConfirmAccount.html"
+    template_name = "page/ConfirmAccount.html"
 
  #Account pages views
 class AccountView(TemplateView):
-    template_name = "main_templates/Account.html"
+    template_name = "page/Account.html"
 class AccountGroupsView(TemplateView):
-    template_name = "main_templates/AccountGroups.html"
+    template_name = "page/AccountGroups.html"
 class AccountSettingsView(TemplateView):
-    template_name = "main_templates/AccountSettings.html"
+    template_name = "page/AccountSettings.html"
 
 #Group pages views
 class GroupView(TemplateView):
-    template_name = "main_templates/Group.html"
+    template_name = "page/Group.html"
 class GroupMembersView(TemplateView):
-    template_name = "main_templates/GroupMembers.html"
+    template_name = "page/GroupMembers.html"
 class GroupMembersAddView(TemplateView):
-    template_name = "main_templates/GroupMembersAdd.html"
+    template_name = "page/GroupMembersAdd.html"
 class GroupBetsView(TemplateView):
-    template_name = "main_templates/GroupBets.html"
+    template_name = "page/GroupBets.html"
 class GroupBetsActiveView(TemplateView):
-    template_name = "main_templates/GroupBetsActive.html"
+    template_name = "page/GroupBetsActive.html"
 class GroupBetsCompletedView(TemplateView):
-    template_name = "main_templates/GroupBetsCompleted.html"
+    template_name = "page/GroupBetsCompleted.html"
 class GroupBetsCreateView(TemplateView):
-    template_name = "main_templates/GroupBetsCreate.html"
+    template_name = "page/GroupBetsCreate.html"
 class GroupBetView(TemplateView):
-    template_name = "main_templates/GroupBet.html"
+    template_name = "page/GroupBet.html"
 class GroupBetAddView(TemplateView):
-    template_name = "main_templates/GroupBetAdd.html"
+    template_name = "page/GroupBetAdd.html"
