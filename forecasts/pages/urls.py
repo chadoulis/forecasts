4b285b73820715132fb6315d6fcf0c93757d64aa
Moutlou <31949397+dhmm@users.noreply.github.com>
2021-09-06 23:14:26 +0300

    [41960055] Create all pages with working urls (#6)
    * links added to layout Root
    
    * general pages added
    
    * Registration and password pages added
    
    * Account pages added
    
    * Group pages added

--- forecasts/pages/urls.py
+++ forecasts/pages/urls.py
@@ -1,8 +1,38 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from forecasts.pages.views import AboutView, HomeView
+from forecasts.pages.views import HomeView, AboutView, TeamView , TermsAndConditionsView , PrivacyPolicyView
+from forecasts.pages.views import LoginView, RegisterView, ForgotPasswordView, ResetPasswordView, ConfirmAccountView
+from forecasts.pages.views import AccountView, AccountGroupsView, AccountSettingsView
+from forecasts.pages.views import GroupView, GroupMembersView, GroupMembersAddView, GroupBetsView, GroupBetsActiveView, GroupBetsCompletedView, GroupBetsCreateView, GroupBetView, GroupBetAddView
 
 urlpatterns = [
+    #General Pages
     path('', HomeView.as_view()),
     path('about/', AboutView.as_view()),
+    path('team/', TeamView.as_view()),
+    path('terms-and-conditions/', TermsAndConditionsView.as_view()),
+    path('privacy-policy/', PrivacyPolicyView.as_view()),
+
+    #Registration and password pages
+    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('forgot-password/', ForgotPasswordView.as_view()),    
+    path('reset-password/<str:TempToken>', ResetPasswordView.as_view()),                        
+    path('confirm-account/<str:TempToken>', ConfirmAccountView.as_view()),     
+
+    #Account pages 
+    path('account/', AccountView.as_view()),
+    path('account/groups/', AccountGroupsView.as_view()),
+    path('account/settings/', AccountSettingsView.as_view()),
+
+    #Group pages    
+    path('group/<str:GroupID>/', GroupView.as_view()),
+    path('group/<str:GroupID>/members/', GroupMembersView.as_view()),
+    path('group/<str:GroupID>/members/add/', GroupMembersAddView.as_view()),
+    path('group/<str:GroupID>/bets/', GroupBetsView.as_view()),
+    path('group/<str:GroupID>/bets/active/', GroupBetsActiveView.as_view()),
+    path('group/<str:GroupID>/bets/completed/', GroupBetsCompletedView.as_view()),
+    path('group/<str:GroupID>/bets/create/', GroupBetsCreateView.as_view()),
+    path('group/<str:GroupID>/bet/<str:BetID>', GroupBetView.as_view()),
+    path('group/<str:GroupID>/bet/<str:BetID>/add/', GroupBetAddView.as_view()),
 ]
--- forecasts/pages/views.py
+++ forecasts/pages/views.py
@@ -2,9 +2,54 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-class AboutView(TemplateView):
-    template_name = "main_templates/About.html"
-
-
+#General views
 class HomeView(TemplateView):
     template_name = "main_templates/Home.html"
+class AboutView(TemplateView):
+    template_name = "main_templates/About.html"
+class TeamView(TemplateView):
+    template_name = "main_templates/Team.html"
+class TermsAndConditionsView(TemplateView):
+    template_name = "main_templates/TermsAndConditions.html"  
+class PrivacyPolicyView(TemplateView):
+    template_name = "main_templates/PrivacyPolicy.html"
+
+#Registration and password views
+class LoginView(TemplateView):
+    template_name = "main_templates/Login.html"
+class RegisterView(TemplateView):
+    template_name = "main_templates/Register.html"
+class ForgotPasswordView(TemplateView):
+    template_name = "main_templates/ForgotPassword.html"
+class ResetPasswordView(TemplateView):
+    template_name = "main_templates/ResetPassword.html"
+class ConfirmAccountView(TemplateView):
+    template_name = "main_templates/ConfirmAccount.html"
+
+ #Account pages views
+class AccountView(TemplateView):
+    template_name = "main_templates/Account.html"
+class AccountGroupsView(TemplateView):
+    template_name = "main_templates/AccountGroups.html"
+class AccountSettingsView(TemplateView):
+    template_name = "main_templates/AccountSettings.html"
+
+#Group pages views
+class GroupView(TemplateView):
+    template_name = "main_templates/Group.html"
+class GroupMembersView(TemplateView):
+    template_name = "main_templates/GroupMembers.html"
+class GroupMembersAddView(TemplateView):
+    template_name = "main_templates/GroupMembersAdd.html"
+class GroupBetsView(TemplateView):
+    template_name = "main_templates/GroupBets.html"
+class GroupBetsActiveView(TemplateView):
+    template_name = "main_templates/GroupBetsActive.html"
+class GroupBetsCompletedView(TemplateView):
+    template_name = "main_templates/GroupBetsCompleted.html"
+class GroupBetsCreateView(TemplateView):
+    template_name = "main_templates/GroupBetsCreate.html"
+class GroupBetView(TemplateView):
+    template_name = "main_templates/GroupBet.html"
+class GroupBetAddView(TemplateView):
+    template_name = "main_templates/GroupBetAdd.html"
